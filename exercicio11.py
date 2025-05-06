5c571c642c75211571bcc97f46d235b9ed5dc293
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-05-06 11:30:32 -0300

    atualizaçaozinha

--- exercicio11.py
+++ exercicio11.py
@@ -1,9 +1,10 @@
 nums = ["","","","",""]
 for i in range(len(nums)):
    nums[i] = int(input("digite os numeros: "))
+print(nums)
 numero = int(input("digite um numero para procurar: "))
 cont = 0
 for x in range(len(nums)):
    if numero == nums[x]:
-       cont =+ 1
+       cont += 1
 print(f"encontrei o {numero} que aparece {cont} vezes")
